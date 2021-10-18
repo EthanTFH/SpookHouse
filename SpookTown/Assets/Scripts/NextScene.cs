f912d51d73e861b8d4278afe71e4fe858301752d
EthanTFH <egibson8202@gmail.com>
2021-10-18 18:44:27 -0500

    Fading to inside the house
    Added a script to fade the player into the house.

---
+++ SpookTown/Assets/Scripts/NextScene.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
+    public bool isPathwayScene = false;
+    public GameObject panel;
+
+    private Scene scene;
+    private bool fadeDone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isPathwayScene)
+        {
+            panel.GetComponent<Image>().color = new Color(panel.GetComponent<Image>().color.r, panel.GetComponent<Image>().color.g, panel.GetComponent<Image>().color.b, 0);
+        }
+
+        scene = SceneManager.GetActiveScene();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator FadeBlack()
+    {
+        Debug.Log("Fading to death");
+        Color objectColor = panel.GetComponent<Image>().color;
+        float fadeAmount;
+        
+        while (panel.GetComponent<Image>().color.a < 1)
+        {
+            controller.m_WalkSpeed = 0f;
+            fadeAmount = objectColor.a + (Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            panel.GetComponent<Image>().color = objectColor;
+            yield return null;
+        }
+        fadeDone = true;
+    }
+
+    public IEnumerator startFade()
+    {
+        yield return FadeBlack();
+
+        int currentScene = scene.buildIndex;
+        SceneManager.LoadScene(currentScene + 1, LoadSceneMode.Single);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger entered");
+        if (other.gameObject.name == "Player")
+        {
+            StartCoroutine("startFade");
+        }
+    }
+}
